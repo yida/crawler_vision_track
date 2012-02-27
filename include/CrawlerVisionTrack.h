bb24621a0f59483eb0600918b4fed61a9a8cd49b
Yida Zhang <yida@seas.upenn.edu>
2012-02-27 14:49:52 -0500

    add image read and launch file

--- include/CrawlerVisionTrack.h
+++ include/CrawlerVisionTrack.h
@@ -1,5 +1,28 @@
 #ifndef __CRAWLER_VISION_TRACK__
 #define __CRAWLER_VISION_TRACK__
 
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <std_msgs/String.h>
+
+class VisionTracker {
+
+public:
+	VisionTracker(ros::NodeHandle &);
+	~VisionTracker() {}
+
+private:
+	image_transport::ImageTransport it_;
+	image_transport::Subscriber SubImage;
+	// Debug Msgs Publish
+	ros::Publisher DebugMsgs;
+
+	void ImageProc(const sensor_msgs::ImageConstPtr& msg);
+	void Tracker();
+
+	sensor_msgs::Image curFrame;
+
+};
 
 #endif
--- src/CrawlerVisionTrack.cpp
+++ src/CrawlerVisionTrack.cpp
@@ -1,6 +1,29 @@
+
 #include <CrawlerVisionTrack.h>
+
+VisionTracker::VisionTracker(ros::NodeHandle& node)
+:it_(node)
+,SubImage(it_.subscribe("/image_in",1,&VisionTracker::ImageProc, this))
+,DebugMsgs(node.advertise<std_msgs::String>("debug",100))
+
+,curFrame()
+{
+}
+
+void VisionTracker::ImageProc(const sensor_msgs::ImageConstPtr& msg)
+{
+	curFrame = *msg;
+	std::cout << curFrame.encoding << std::endl;
+}
+
+void VisionTracker::Tracker()
+{
+}
 
 int main(int argc, char **argv) 
 {
+	ros::init(argc, argv, "crawler_vision_track");
+	ros::NodeHandle nh_("/crawler_vision_track");
+	VisionTracker tracker(nh_);
 	return 0;
 }
